3b9d21411a071be314c85d8de7919e4599b6c212
migraineman <migraineman@migraineman.net>
2025-01-08 12:26:25 -0600

    stage 1 of project finished

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,5 +1,13 @@
 package parking
 
 fun main() {
+    var carColor = readln()
+    println("$carColor car has parked.")
+
+    carColor = readln()
+    println("$carColor car left the parking lot.")
+
+    carColor = readln()
+    println("$carColor car just parked here.")
 
 }
